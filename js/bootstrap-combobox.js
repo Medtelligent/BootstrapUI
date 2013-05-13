5057652a675e5b417d0d0f11354668cfc5630988
Huzaifa Tapal <huzaifa.tapal@medtelligent.com>
2013-05-13 16:28:02 -0500

    Formatted code to correct indentation

--- js/bootstrap-combobox.js
+++ js/bootstrap-combobox.js
@@ -40,275 +40,274 @@
 
         constructor: Combobox
 
-  , select: function () {
-      var val = this.$menu.find('.active').attr('data-value')
-      this.$element
-        .val(this.updater(val))
-        .change()
-      return this.hide()
-  }
+        , select: function () {
+            var val = this.$menu.find('.active').attr('data-value')
+            this.$element
+              .val(this.updater(val))
+              .change()
+            return this.hide()
+        }
 
-  , updater: function (item) {
-      return item
-  }
+        , updater: function (item) {
+            return item
+        }
 
-  , show: function () {
-      var pos = $.extend({}, this.$element.offset(), {
-          height: this.$element[0].offsetHeight
-      })
+        , show: function () {
+            var pos = $.extend({}, this.$element.offset(), {
+                height: this.$element[0].offsetHeight
+            })
 
-      this.$menu
-        .addClass('qs-combobox-menu')
-        .appendTo('body')
-        .css({
-            top: pos.top + pos.height
-        , left: pos.left
-        , minWidth: this.$element.outerWidth() + 'px'
-        , zIndex: '100000000000'
-        })
-        .show()
+            this.$menu
+              .addClass('qs-combobox-menu')
+              .appendTo('body')
+              .css({
+                  top: pos.top + pos.height
+              , left: pos.left
+              , minWidth: this.$element.outerWidth() + 'px'
+              , zIndex: '100000000000'
+              })
+              .show()
 
-      if (!this.$menu.attr('id')) {
-          this.$menu.attr('id', 'qs-combobox-menu-' + $('body > .qs-combobox-menu').length)
-      }
+            if (!this.$menu.attr('id')) {
+                this.$menu.attr('id', 'qs-combobox-menu-' + $('body > .qs-combobox-menu').length)
+            }
 
-      this.shown = true
-      return this
-  }
+            this.shown = true
+            return this
+        }
 
-  , hide: function () {
-      this.$menu.hide()
-      this.shown = false
-      return this
-  }
+        , hide: function () {
+            this.$menu.hide()
+            this.shown = false
+            return this
+        }
 
-  , lookup: function (event) {
-      var items
+        , lookup: function (event) {
+            var items
 
-      this.query = !event || !$(event.currentTarget).is('[data-toggle=menu]') ? this.$element.val() : null;
+            this.query = !event || !$(event.currentTarget).is('[data-toggle=menu]') ? this.$element.val() : null;
 
-      if (this.query && this.query.length < this.options.minLength) {
-          return this.shown ? this.hide() : this
-      }
+            if (this.query && this.query.length < this.options.minLength) {
+                return this.shown ? this.hide() : this
+            }
 
-      items = $.isFunction(this.source) ? this.source(this.query, $.proxy(this.process, this)) : this.source
+            items = $.isFunction(this.source) ? this.source(this.query, $.proxy(this.process, this)) : this.source
 
-      return items ? this.process(items) : this
-  }
+            return items ? this.process(items) : this
+        }
 
-  , process: function (items) {
-      var that = this
+        , process: function (items) {
+            var that = this
 
-      items = $.grep(items, function (item) {
-          return that.query ? that.matcher(item) : true;
-      })
+            items = $.grep(items, function (item) {
+                return that.query ? that.matcher(item) : true;
+            })
 
-      items = this.sorter(items)
+            items = this.sorter(items)
 
-      if (!items.length) {
-          return this.shown ? this.hide() : this
-      }
+            if (!items.length) {
+                return this.shown ? this.hide() : this
+            }
 
-      return this.render(items.slice(0, this.options.items)).show()
-  }
+            return this.render(items.slice(0, this.options.items)).show()
+        }
 
-  , matcher: function (item) {
-      return ~item.toLowerCase().indexOf(this.query.toLowerCase())
-  }
+        , matcher: function (item) {
+            return ~item.toLowerCase().indexOf(this.query.toLowerCase())
+        }
 
-  , sorter: function (items) {
-      var beginswith = []
-        , caseSensitive = []
-        , caseInsensitive = []
-        , item
+        , sorter: function (items) {
+            var beginswith = []
+              , caseSensitive = []
+              , caseInsensitive = []
+              , item
 
-      while (item = items.shift()) {
-          if (this.query && !item.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
-          else if (this.query && ~item.indexOf(this.query)) caseSensitive.push(item)
-          else caseInsensitive.push(item)
-      }
+            while (item = items.shift()) {
+                if (this.query && !item.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
+                else if (this.query && ~item.indexOf(this.query)) caseSensitive.push(item)
+                else caseInsensitive.push(item)
+            }
 
-      return beginswith.concat(caseSensitive, caseInsensitive)
-  }
+            return beginswith.concat(caseSensitive, caseInsensitive)
+        }
 
-  , highlighter: function (item) {
-      if (this.query) {
-          var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
-          return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
-              return '<strong>' + match + '</strong>'
-          })
-      }
-      return item;
-  }
+        , highlighter: function (item) {
+            if (this.query) {
+                var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
+                return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
+                    return '<strong>' + match + '</strong>'
+                })
+            }
+            return item;
+        }
 
-  , render: function (items) {
-      var that = this
+        , render: function (items) {
+            var that = this
 
-      items = $(items).map(function (i, item) {
-          i = $(that.options.item).attr('data-value', item)
-          i.find('a').html(that.highlighter(item))
-          return i[0]
-      })
+            items = $(items).map(function (i, item) {
+                i = $(that.options.item).attr('data-value', item)
+                i.find('a').html(that.highlighter(item))
+                return i[0]
+            })
 
-      items.first().addClass('active')
-      this.$menu.html(items)
-      return this
-  }
+            items.first().addClass('active')
+            this.$menu.html(items)
+            return this
+        }
 
-  , next: function (event) {
-      var active = this.$menu.find('.active').removeClass('active')
-        , next = active.next()
+        , next: function (event) {
+            var active = this.$menu.find('.active').removeClass('active')
+              , next = active.next()
 
-      if (!next.length) {
-          next = $(this.$menu.find('li')[0])
-      }
+            if (!next.length) {
+                next = $(this.$menu.find('li')[0])
+            }
 
-      next.addClass('active')
-  }
+            next.addClass('active')
+        }
 
-  , prev: function (event) {
-      var active = this.$menu.find('.active').removeClass('active')
-        , prev = active.prev()
+        , prev: function (event) {
+            var active = this.$menu.find('.active').removeClass('active')
+              , prev = active.prev()
 
-      if (!prev.length) {
-          prev = this.$menu.find('li').last()
-      }
+            if (!prev.length) {
+                prev = this.$menu.find('li').last()
+            }
 
-      prev.addClass('active')
-  }
+            prev.addClass('active')
+        }
 
-  , listen: function () {
-      this.$element
-        .on('focus', $.proxy(this.focus, this))
-        .on('blur', $.proxy(this.blur, this))
-        .on('keypress', $.proxy(this.keypress, this))
-        .on('keyup', $.proxy(this.keyup, this))
+        , listen: function () {
+            this.$element
+              .on('focus', $.proxy(this.focus, this))
+              .on('blur', $.proxy(this.blur, this))
+              .on('keypress', $.proxy(this.keypress, this))
+              .on('keyup', $.proxy(this.keyup, this))
 
-      if (this.eventSupported('keydown')) {
-          this.$element.on('keydown', $.proxy(this.keydown, this))
-      }
+            if (this.eventSupported('keydown')) {
+                this.$element.on('keydown', $.proxy(this.keydown, this))
+            }
 
-      this.$menu
-        .on('click', $.proxy(this.click, this))
-        .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
-        .on('mouseleave', 'li', $.proxy(this.mouseleave, this))
+            this.$menu
+              .on('click', $.proxy(this.click, this))
+              .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
+              .on('mouseleave', 'li', $.proxy(this.mouseleave, this))
 
-      this.$trigger
-        .on('click', $.proxy(this.triggerClick, this))
+            this.$trigger
+              .on('click', $.proxy(this.triggerClick, this))
 
-      $('html').on('click.dropdown.data-api', $.proxy(function () {
-          this.hide();
-      }, this))
-  }
+            $('html').on('click.dropdown.data-api', $.proxy(function () {
+                this.hide();
+            }, this))
+        }
 
-  , eventSupported: function (eventName) {
-      var isSupported = eventName in this.$element
-      if (!isSupported) {
-          this.$element.setAttribute(eventName, 'return;')
-          isSupported = typeof this.$element[eventName] === 'function'
-      }
-      return isSupported
-  }
+        , eventSupported: function (eventName) {
+            var isSupported = eventName in this.$element
+            if (!isSupported) {
+                this.$element.setAttribute(eventName, 'return;')
+                isSupported = typeof this.$element[eventName] === 'function'
+            }
+            return isSupported
+        }
 
-  , move: function (e) {
+        , move: function (e) {
 
-      switch (e.keyCode) {
-          case 9: // tab
-          case 13: // enter
-          case 27: // escape
-              e.preventDefault()
-              break
+            switch (e.keyCode) {
+                case 9: // tab
+                case 13: // enter
+                case 27: // escape
+                    e.preventDefault()
+                    break
 
-          case 38: // up arrow
-              e.preventDefault()
-              if (this.shown) this.prev()
-              break
+                case 38: // up arrow
+                    e.preventDefault()
+                    if (this.shown) this.prev()
+                    break
 
-          case 40: // down arrow
-              e.preventDefault()
-              if (this.shown) this.next()
-              else {
-                  this.focused = true;
-                  this.lookup();
-              }
-              break
-      }
+                case 40: // down arrow
+                    e.preventDefault()
+                    if (this.shown) this.next()
+                    else {
+                        this.focused = true;
+                        this.lookup();
+                    }
+                    break
+            }
 
-      e.stopPropagation()
-  }
+            e.stopPropagation()
+        }
 
-  , keydown: function (e) {
-      this.suppressKeyPressRepeat = ~$.inArray(e.keyCode, [40, 38, 9, 13, 27])
-      this.move(e)
-  }
+        , keydown: function (e) {
+            this.suppressKeyPressRepeat = ~$.inArray(e.keyCode, [40, 38, 9, 13, 27])
+            this.move(e)
+        }
 
-  , keypress: function (e) {
-      if (this.suppressKeyPressRepeat) return
-      this.move(e)
-  }
+        , keypress: function (e) {
+            if (this.suppressKeyPressRepeat) return
+            this.move(e)
+        }
 
-  , keyup: function (e) {
-      switch (e.keyCode) {
-          case 40: // down arrow
-          case 38: // up arrow
-          case 16: // shift
-          case 17: // ctrl
-          case 18: // alt
-              break
+        , keyup: function (e) {
+            switch (e.keyCode) {
+                case 40: // down arrow
+                case 38: // up arrow
+                case 16: // shift
+                case 17: // ctrl
+                case 18: // alt
+                    break
 
-          case 9: // tab
-          case 13: // enter
-              if (!this.shown) return
-              this.select()
-              break
+                case 9: // tab
+                case 13: // enter
+                    if (!this.shown) return
+                    this.select()
+                    break
 
-          case 27: // escape
-              if (!this.shown) return
-              this.hide()
-              break
+                case 27: // escape
+                    if (!this.shown) return
+                    this.hide()
+                    break
 
-          default:
-              this.lookup()
-      }
+                default:
+                    this.lookup()
+            }
 
-      e.stopPropagation()
-      e.preventDefault()
-  }
+            e.stopPropagation()
+            e.preventDefault()
+        }
 
-  , focus: function (e) {
-      this.focused = true
-  }
+        , focus: function (e) {
+            this.focused = true
+        }
 
-  , blur: function (e) {
-      this.focused = false
-      if (!this.mousedover && this.shown) this.hide()
-  }
+        , blur: function (e) {
+            this.focused = false
+            if (!this.mousedover && this.shown) this.hide()
+        }
 
-  , click: function (e) {
-      e.stopPropagation()
-      e.preventDefault()
-      this.select()
-      this.$element.focus()
-  }
+        , click: function (e) {
+            e.stopPropagation()
+            e.preventDefault()
+            this.select()
+            this.$element.focus()
+        }
 
-  , triggerClick: function (e) {
-      e.stopPropagation()
-      e.preventDefault()
-      this.focused = true;
-      this.lookup(e);
-  }
+        , triggerClick: function (e) {
+            e.stopPropagation()
+            e.preventDefault()
+            this.focused = true;
+            this.lookup(e);
+        }
 
-  , mouseenter: function (e) {
-      this.mousedover = true
-      this.$menu.find('.active').removeClass('active')
-      $(e.currentTarget).addClass('active')
-  }
+        , mouseenter: function (e) {
+            this.mousedover = true
+            this.$menu.find('.active').removeClass('active')
+            $(e.currentTarget).addClass('active')
+        }
 
-  , mouseleave: function (e) {
-      this.mousedover = false
-      if (!this.focused && this.shown) this.hide()
-  }
-
+        , mouseleave: function (e) {
+            this.mousedover = false
+            if (!this.focused && this.shown) this.hide()
+        }
     }
 
 
@@ -329,10 +328,10 @@
 
     $.fn.combobox.defaults = {
         source: []
-  , items: 8
-  , menu: '<ul class="combobox dropdown-menu"></ul>'
-  , item: '<li><a href="#"></a></li>'
-  , minLength: 1
+        , items: 8
+        , menu: '<ul class="combobox dropdown-menu"></ul>'
+        , item: '<li><a href="#"></a></li>'
+        , minLength: 1
     }
 
     $.fn.combobox.Constructor = Combobox
